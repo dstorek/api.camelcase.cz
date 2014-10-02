823df42d25a0fa95345e8a1aabd15e1e4da1492e
Daniel Storek <dstorek@post.cz>
2014-10-02 08:42:32 +0200

    added logging passkit errors

--- models/log.js
+++ models/log.js
@@ -3,7 +3,11 @@ var mongoose =  require('mongoose');
 // define the schema for Apple Passkit service logs
 var logSchema = mongoose.Schema({
 
-    logs: { type : Array, default : [] }
+    // logs: { type : Array, default : [] }
+
+    lastUpdated: {type: String, default : "today"},         // used only for convenience as a human readable format
+    lastUpdatedTimestamp: {type: String, default : 0 },
+    logs: {type: Array }
 
 },{ collection : 'logs' });
 
--- passkit/logPasskitErrors.js
+++ passkit/logPasskitErrors.js
@@ -10,6 +10,23 @@ var logPasskitErrors = function(req, res){
         console.log(logs);
     }
 
+    var lastUpdated         = new Date().toJSON();      // ISO8601 formatted timestamp
+    var lastUpdatedTimestamp  = Date.now();             //  timestamp in ms from 1970
+
+    // creates and saves a new LOg record in the database
+    var newLog                      = new Log;
+    newLog.lastUpdated              = lastUpdated;
+    newLog.lastUpdatedTimestamp     = lastUpdatedTimestamp;
+    newLog.logs                     = logs;
+
+    // save the Passbook
+    newLog.save(function(err) {
+        if (err) {
+            // res.send(404);
+            throw err;
+        }
+    });
+
     res.send(200);
     /*
     Log.find({ 'pass.passTypeIdentifier' : passTypeIdentifier, devices : { $elemMatch: { deviceLibraryIdentifier: deviceLibraryIdentifier } } },
